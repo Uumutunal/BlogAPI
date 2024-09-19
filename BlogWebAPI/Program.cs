77678db98736f2a0ff5b114df6a6bd407ffff937
yigitcicekk <ygtcck.67@gmail.com>
2024-09-19 14:32:33 +0300

    deployee

--- BlogWebAPI/Program.cs
+++ BlogWebAPI/Program.cs
@@ -112,7 +112,7 @@ namespace BlogWebAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
